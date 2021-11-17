d3e1c81c1994367558554ee3ae8784e6e4da379f
beccamason <beccamason28@gmail.com>
2021-11-17 12:15:31 +0000

    Code finding correct email addresses

--- 2EmailExtraction/Program.cs
+++ 2EmailExtraction/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+using System.IO;
 
 namespace _2EmailExtraction
 {
@@ -6,7 +8,18 @@ namespace _2EmailExtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string emails = "";
+            string text = File.ReadAllText(@"C:\Users\Rebecca.Mason\Documents\Corndel\SupportDocs\sample.txt");
+            Regex rg = new Regex(@"([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)");
+            MatchCollection match = rg.Matches(text);
+            
+            for (int count = 0; count < match.Count; count++)
+            {
+                emails = emails + ", " + match[count].Value;
+            }
+            Console.WriteLine(emails);
         }
+
     }
+
 }
